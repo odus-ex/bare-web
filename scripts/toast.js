9394b79910650ad3c87bbe8cc5cbe19bbfbe64c1
odus-ex <at.infosec@protonmail.com>
2022-05-12 17:29:48 +0530

    merged with remote changes

--- scripts/toast.js
+++ scripts/toast.js
@@ -1,14 +1,12 @@
-let TOAST_COUNT = 0;
 function createToast() {
   let motherDiv = document.getElementById("motherToast");
 
   let toast = document.createElement("div");
   toast.setAttribute("class", "toast-wrapper toast-fade-in");
-  TOAST_COUNT += 1;
-  toast.innerHTML = `This is a toast ${TOAST_COUNT}`;
+
+  toast.innerHTML = `This is a toast`;
 
   toast.addEventListener("click", () => toast.remove());
-  console.log(toast);
 
   setTimeout(() => {
     toast.setAttribute("class", "toast-wrapper toast-fade-out");
